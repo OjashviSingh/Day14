2eb07edb56004df0d57b824c0b9927b0c5ad3beb
OjashviSingh <ojashvisingh365@gmail.com>
2023-03-11 14:11:08 +0530

    Added: UC1

---
+++ LinkedList1.java
@@ -0,0 +1,46 @@
+public class LinkedList1 {
+
+    Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    public void addNode(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList1 ll = new LinkedList1();
+
+        ll.addNode(56);
+        ll.addNode(30);
+        ll.addNode(70);
+
+        ll.printList();
+    }
+}
