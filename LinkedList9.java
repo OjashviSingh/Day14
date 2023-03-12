cd3287c129ccff855ad32cbcc677402cdb642abc
OjashviSingh <ojashvisingh365@gmail.com>
2023-03-12 11:44:33 +0530

    Added: UC9

---
+++ LinkedList9.java
@@ -0,0 +1,82 @@
+public class LinkedList9 {
+
+    Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    public void addNode(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    public void deleteNode(int key) {
+        Node temp = head;
+        Node prev = null;
+
+        if (temp != null && temp.data == key) {
+            head = temp.next;
+            return;
+        }
+
+        while (temp != null && temp.data != key) {
+            prev = temp;
+            temp = temp.next;
+        }
+
+        if (temp == null) {
+            return;
+        }
+
+        prev.next = temp.next;
+    }
+
+    public int size() {
+        int count = 0;
+        Node temp = head;
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList9 ll = new LinkedList9();
+
+        ll.addNode(56);
+        ll.addNode(30);
+        ll.addNode(40);
+        ll.addNode(70);
+
+        ll.deleteNode(40);
+
+        System.out.println("Linked List size: " + ll.size());
+
+        ll.printList();
+    }
+}
